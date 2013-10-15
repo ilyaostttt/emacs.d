f336b5a619be5b7752565410c0e03d5d284b9270
ilya <ilya@ilya-VirtualBox.(none)>
2013-10-15 18:17:41 +0400

    quantenna related configuration

---
+++ rc/rc-qtn.el
@@ -0,0 +1,8 @@
+(setq default-tab-width 8
+      c-default-style "linux")
+(setq-default c-basic-offset 8)
+(setq-default show-trailing-whitespace t)
+
+(set-default 'indent-tabs-mode t)
+
+(provide 'rc-qtn)
