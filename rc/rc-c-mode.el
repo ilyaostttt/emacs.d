eab46573574699e4008e8595d0063201d9a368d4
Ilya Ostanin <ilyaostttt@gmail.com>
2015-09-25 15:34:49 -0700

    C style based on the code from the Linux CodeStyle.

--- rc/rc-c-mode.el
+++ rc/rc-c-mode.el
@@ -1,5 +1,6 @@
+(setq c-default-style "linux")
+
 (add-hook 'c-mode-common-hook (lambda ()
-				;; (semantic-initialize)
 				(fci-mode)
 				(set-fill-column 100)))
 
@@ -7,6 +8,29 @@
 				      (fci-mode)
 				      (set-fill-column 100)))
 
+(defun c-lineup-arglist-tabs-only (ignored)
+  "Line up argument lists by tabs, not spaces"
+  (let* ((anchor (c-langelem-pos c-syntactic-element))
+         (column (c-langelem-2nd-pos c-syntactic-element))
+         (offset (- (1+ column) anchor))
+         (steps (floor offset c-basic-offset)))
+    (* (max steps 1)
+       c-basic-offset)))
 
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            ;; Add kernel style
+            (c-add-style
+             "linux-tabs-only"
+             '("linux" (c-offsets-alist
+                        (arglist-cont-nonempty
+                         c-lineup-gcc-asm-reg
+                         c-lineup-arglist-tabs-only))))))
+
+(add-hook 'c-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode t)
+	    (setq show-trailing-whitespace t)
+	    (c-set-style "linux-tabs-only")))
 
 (provide 'rc-c-mode)
