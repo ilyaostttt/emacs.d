a709316fcd620bf71bc0c4e52898367f79218590
iostanin <iostanin@quantenna.com>
2020-03-12 12:55:07 +0300

    Enable dired-omit-mode

--- elisp/dired-setup.el
+++ elisp/dired-setup.el
@@ -1,1 +1,10 @@
+(load "dired-x")
+(load "dired-x")
+
+(add-hook 'dired-mode-hook
+	  (lambda ()
+	    ;; Set dired-x buffer-local variables here.  For example:
+	    (dired-omit-mode 1)
+	    ))
+
 (provide 'dired-setup)
